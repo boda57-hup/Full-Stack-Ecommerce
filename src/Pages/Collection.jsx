11476a4590681a60ecf5f221180fec49b4f97873
Abdallah Adel <01159355023am@gmail.com>
2025-12-05 19:30:19 +0200

    update filter and update search and update sort

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -10,6 +10,7 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCatogory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relavent');
 
   const toggleCategory = (e) => {
     if (category.includes(e.target.value)) {
@@ -21,9 +22,9 @@ const Collection = () => {
 
   const togglesubcategory = (e) => {
     if (subCategory.includes(e.target.value)) {
-      setCatogory(prev => prev.filter(item => item !== e.target.value))
+      setSubCategory(prev => prev.filter(item => item !== e.target.value));
     } else {
-      setSubCategory(prev => [...prev, e.target.value])
+      setSubCategory(prev => [...prev, e.target.value]);
     }
   }
 
@@ -33,21 +34,40 @@ const Collection = () => {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
 
-    if(subCategory.length > 0){
+    if (subCategory.length > 0) {
       productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory))
     }
 
     setFilterProducts(productsCopy)
   }
 
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
 
-  useEffect(() => {
-    setFilterProducts(products);
-  })
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a, b) => (a.price - b.price)));
+        break;
+
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a, b) => (b.price - a.price)));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+  }
+
+
 
   useEffect(() => {
     applyFilter(products);
   }, [category, subCategory])
+
+  useEffect(()=>{
+    sortProduct();
+  },[sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -94,7 +114,7 @@ const Collection = () => {
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'} />
           {/* Product Sort */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
             <option value='relavent'>sort by : Relavent</option>
             <option value='low-high'> sort by : Low to high </option>
             <option value='high-low'>sort by : High to low</option>
